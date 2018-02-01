24ca82117fa4931b665f24810f0763e0056743ef
xhjdram <1205391574@qq.com>
2018-02-01 18:45:28 +0800

    Initial commit

---
+++ e3-manager/e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
@@ -0,0 +1,22 @@
+
+package cn.e3mall.controller;
+
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.servcice.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class ItemController {
+   @Autowired
+   ItemService itemService;
+    @RequestMapping("/item/{itemId}")
+    @ResponseBody
+    private TbItem getItemById(@PathVariable Long itemId) {
+        TbItem tbItem = itemService.getItemById(itemId);
+        return tbItem;
+
+    }
+}
---
+++ e3-manager/e3-manager-web/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Hope
+  Date: 2018/2/1
+  Time: 1:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>九牛二虎</h1>
+</body>
+</html>
